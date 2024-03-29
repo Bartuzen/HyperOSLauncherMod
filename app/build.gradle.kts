60558704ac02242d4f8e7241c8e97ebdd41f3654
Bartu Özen <git@bartuzen.dev>
2024-03-29 21:29:16 +0300

    Version 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "dev.bartuzen.hyperos_launcher_mod"
         minSdk = 21
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
